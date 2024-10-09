e7348eb3e2119d286c5c0c4d5ab194e182ea64ba
Jiri Petrlik <jiripetrlik@gmail.com>
2024-10-09 09:26:28 +0200

    CARRY: Use environment variable to skip jobset availability check

--- test/util/e2e.go
+++ test/util/e2e.go
@@ -210,8 +210,11 @@ func WaitForAppWrapperAvailability(ctx context.Context, k8sClient client.Client)
 }
 
 func WaitForJobSetAvailability(ctx context.Context, k8sClient client.Client) {
-	jcmKey := types.NamespacedName{Namespace: "jobset-system", Name: "jobset-controller-manager"}
-	waitForOperatorAvailability(ctx, k8sClient, jcmKey)
+	_, skipJobsetAvailabilityCheck := os.LookupEnv("SKIP_JOB_SET_AVAILABILITY_CHECK")
+	if !skipJobsetAvailabilityCheck {
+		jcmKey := types.NamespacedName{Namespace: "jobset-system", Name: "jobset-controller-manager"}
+		waitForOperatorAvailability(ctx, k8sClient, jcmKey)
+	}
 }
 
 func WaitForLeaderWorkerSetAvailability(ctx context.Context, k8sClient client.Client) {
@@ -220,13 +223,19 @@ func WaitForLeaderWorkerSetAvailability(ctx context.Context, k8sClient client.Cl
 }
 
 func WaitForKubeFlowTrainingOperatorAvailability(ctx context.Context, k8sClient client.Client) {
-	kftoKey := types.NamespacedName{Namespace: "kubeflow", Name: "training-operator"}
-	waitForOperatorAvailability(ctx, k8sClient, kftoKey)
+	_, skipTrainingOperatorAvailabilityCheck := os.LookupEnv("SKIP_TRAINING_OPERATOR_AVAILABILITY_CHECK")
+	if !skipTrainingOperatorAvailabilityCheck {
+		kftoKey := types.NamespacedName{Namespace: "kubeflow", Name: "training-operator"}
+		waitForOperatorAvailability(ctx, k8sClient, kftoKey)
+	}
 }
 
 func WaitForKubeFlowMPIOperatorAvailability(ctx context.Context, k8sClient client.Client) {
-	kftoKey := types.NamespacedName{Namespace: "mpi-operator", Name: "mpi-operator"}
-	waitForOperatorAvailability(ctx, k8sClient, kftoKey)
+	_, skipMPIOperatorAvailabilityCheck := os.LookupEnv("SKIP_MPI_OPERATOR_AVAILABILITY_CHECK")
+	if !skipMPIOperatorAvailabilityCheck {
+		kftoKey := types.NamespacedName{Namespace: "mpi-operator", Name: "mpi-operator"}
+		waitForOperatorAvailability(ctx, k8sClient, kftoKey)
+	}
 }
 
 func WaitForKubeRayOperatorAvailability(ctx context.Context, k8sClient client.Client) {
